5998d5ed60c35341bf8cdcaedaf52cac987c475d
Hello World! <82676751+slopoje@users.noreply.github.com>
2021-05-02 22:11:01 +0200

    Create BorrarContacto.php

---
+++ BorrarContacto.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+<p>
+<h1>¡Borrar contacto no está implementada!</h1>
+<h1>¿Te atreves con ella?</h1>
+</p>
+<br>
+<a href='http://1daw.onlinewebshop.net/agenda.php'>Volver a la agenda</a>
+</body>
+</html>
